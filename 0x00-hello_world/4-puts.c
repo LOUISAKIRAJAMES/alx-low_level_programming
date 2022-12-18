71a21d1831ddb7b32ebf8cfdd2c54da8a82388c6
LOUISAKIRAJAMES <LOUISAKIRAJAMES>
2022-12-18 15:56:57 -0800

    Damn man

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+1  #include <stdio.h>
+2
+3  /**
+4   * main - entry point
+5   *
+6   * Return Always 0 (Success)
+7   */
+8  int main(void)
+9  {
+10         puts("\"programming is like building a multilingual puzzle");
+11         return (0);
+12 }
