f0ebecfb8355b7ba99e1e41a24958070e865f780
LOUISAKIRAJAMES <LOUISAKIRAJAMES>
2022-12-18 16:07:23 -0800

    Drugs in them

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+1      #include <stdio.h>
+2
+3      /**
+4       * main - entry point
+5       *
+6       * Return: Always 0 (if successful)
+7       */
+8      int main(void)
+9      {
+10              printf("with proper grammar, but the outcome is a piece of art,\n");
+11              return (0) ;
+12     }
