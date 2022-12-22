3b9b5eae5ea46afddb4cbc509154019f6c55b079
LOUISAKIRAJAMES <LOUISAKIRAJAMES>
2022-12-22 01:58:25 -0800

    New han

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-1      #include <stdio.h>
-2
-3      /**
-4       * main - entry point
-5       *
-6       * Return: Always 0 (if successful)
-7       */
-8      int main(void)
-9      {
-10              printf("with proper grammar, but the outcome is a piece of art,\n");
-11              return (0) ;
-12     }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *  Return: Always 0 (if Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
